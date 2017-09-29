43c5534283f62c6d639ebf61f187968ec76ac830
Sushant More <more.13@osu.edu>
2017-09-28 23:52:51 -0400

    Value for the highest individual sale for each store

--- salesData/reducer.py
+++ salesData/reducer.py
@@ -1,30 +1,37 @@
-salesTotal = 0.0
+#!/usr/bin/python
+
+import sys
+
+salesMax = 0.0
 oldKey = None
 
 # Loop around the data
-# It will be in the format key \t val as outputted from the mapper (and also sorted)
+# It will be in the format key\tval
 # Where key is the store name, val is the sale amount
 #
 # All the sales for a particular store will be presented,
 # then the key will change and we'll be dealing with the next store
 
 for line in sys.stdin:
-    dataMapped = line.strip().split("\t")
-    if len(dataMapped) != 2:
+    data_mapped = line.strip().split("\t")
+    if len(data_mapped) != 2:
         # Something has gone wrong. Skip this line.
         continue
 
-    thisKey, thisSale = dataMapped
+    thisKey, thisSale = data_mapped
 
     if oldKey and oldKey != thisKey:
-        print oldKey, "\t", salesTotal
+        print oldKey, "\t", salesMax
         oldKey = thisKey;
-        salesTotal = 0
+        salesMax = 0
 
     oldKey = thisKey
-    salesTotal += float(thisSale)
-    # converting to float is necessary because thisSale is a string.
+
+    if float(thisSale) >= salesMax:
+	
+    	salesMax = float(thisSale)
 
 if oldKey != None:
-    print oldKey, "\t", salesTotal
+    print oldKey, "\t", salesMax
+
 
