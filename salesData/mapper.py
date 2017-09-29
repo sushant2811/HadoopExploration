68f65f3544a370858bafc1eefb0e275665fa5783
Sushant More <more.13@osu.edu>
2017-09-28 23:46:39 -0400

    Mappers and reducers for getting Total sales for a given store

--- salesData/mapper.py
+++ salesData/mapper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 # Format of each line is:
-# date\ttime\tstore name\titem description\tcost\tmethod of payment
+# date \t time \t storeName \t itemDescription \t cost \t methodOfPayment
 #
 # We want elements 2 (store name) and 4 (cost)
 # We need to write them out to standard output, separated by a tab
--- salesData/reducer.py
+++ salesData/reducer.py
@@ -1,14 +1,20 @@
+salesTotal = 0.0
+oldKey = None
+
+# Loop around the data
+# It will be in the format key \t val as outputted from the mapper (and also sorted)
+# Where key is the store name, val is the sale amount
 #
 # All the sales for a particular store will be presented,
 # then the key will change and we'll be dealing with the next store
 
 for line in sys.stdin:
-    data_mapped = line.strip().split("\t")
-    if len(data_mapped) != 2:
+    dataMapped = line.strip().split("\t")
+    if len(dataMapped) != 2:
         # Something has gone wrong. Skip this line.
         continue
 
-    thisKey, thisSale = data_mapped
+    thisKey, thisSale = dataMapped
 
     if oldKey and oldKey != thisKey:
         print oldKey, "\t", salesTotal
@@ -17,6 +23,7 @@ for line in sys.stdin:
 
     oldKey = thisKey
     salesTotal += float(thisSale)
+    # converting to float is necessary because thisSale is a string.
 
 if oldKey != None:
     print oldKey, "\t", salesTotal
