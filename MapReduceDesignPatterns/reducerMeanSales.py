136df5525824b4f24ec22a07fbf417729cf03f19
Sushant More <more.13@osu.edu>
2017-09-30 16:48:20 -0400

    Reducer to calculate mean sales for a given weekday

---
+++ MapReduceDesignPatterns/reducerMeanSales.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import sys
+
+salesTotal = 0.0
+oldKey = None
+salesNumber = 0
+
+# Loop around the data
+# It will be in the format key\tval
+# Where key is the store name, val is the sale amount
+#
+# All the sales for a particular store will be presented,
+# then the key will change and we'll be dealing with the next store
+
+for line in sys.stdin:
+    data_mapped = line.strip().split("\t")
+    if len(data_mapped) != 2:
+        # Something has gone wrong. Skip this line.
+        continue
+
+    thisKey, thisSale = data_mapped
+
+    if oldKey and oldKey != thisKey:
+        print oldKey, "\t", salesTotal / salesNumber
+        oldKey = thisKey;
+        salesTotal = 0
+	salesNumber = 0
+
+    oldKey = thisKey
+    salesTotal += float(thisSale)
+    salesNumber += 1	
+
+if oldKey != None:
+    print oldKey, "\t", salesTotal / salesNumber
+
